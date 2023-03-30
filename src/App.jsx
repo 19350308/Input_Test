23288fc1f8037919f972d7f3b6c780cc10fce81c
19350308 <isc19350308@gmail.com>
2023-03-30 09:05:47 -0600

    Input Inicial con Mensaje Hola

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+
+function App() {
+  const [inputValue, setInputValue]=useState("Hola")
+  const handleInputChange=(e) =>{
+    setInputValue(e.target.value);
+    console.log(e.target.value);
+  }
+  return (
+    <div style={{textAlign: "center"}}>
+      <h1>Input Test</h1>
+      <hr />
+      <input
+      onChange={(e) => handleInputChange(e)}
+      type="text"
+      value={(inputValue)}
+      />
+    </div>
+  )
+}
+
+export default App
