83b447d04985fee03d275dff861920a47d87757c
19350308 <isc19350308@gmail.com>
2023-03-30 09:36:31 -0600

    Configuraciones y Agregamos Input 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import { useState } from 'react';
-
-
+import { useState } from "react";
 function App() {
-  const [inputValue, setInputValue]=useState("Hola")
-  const handleInputChange=(e) =>{
-    setInputValue(e.target.value);
-    console.log(e.target.value);
-  }
-  return (
-    <div style={{textAlign: "center"}}>
-      <h1>Input Test</h1>
-      <hr />
-      <input
-      onChange={(e) => handleInputChange(e)}
-      type="text"
-      value={(inputValue)}
-      />
-    </div>
-  )
-}
+  const [inputValue, setInputValue] = useState({
+    input1: "",
+    input2: "",
+  });
 
+  const handleInputChange = (e) => {
+  setInputValue ({
+    ...inputValue,
+    [e.target.name]: e.target.value,
+  });
+  }
+
+return(
+  <div style={{textAlign: "center"}}>
+  <h1>Input Test</h1>
+  <hr />
+  <label htmlFor="">Input1</label>
+  <input
+  id="in1"
+  name="input1"
+  onChange={(e)=>handleInputChange(e)}
+  type="text"
+  value={inputValue.input1} 
+   />
+  <hr />
+  <label>
+    Input2
+   <input
+   className="mt-2"
+  name="input2"
+  onChange={(e)=>handleInputChange(e)}
+  placeholder='Este es el input2'
+  type="text"
+  value={inputValue.input2} 
+   />
+   </label>
+  </div>
+)
+
+}
 export default App
